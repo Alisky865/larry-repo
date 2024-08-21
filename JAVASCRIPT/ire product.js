559fcea17955a377309723bbe8fed2c339d8f403
alisky865 <aliuyahaya96@gmail.com>
2024-08-21 16:53:40 +0100

    change file directory

--- JAVASCRIPT/ire product.js
+++ JAVASCRIPT/ire product.js
@@ -2,128 +2,148 @@
 
  //import {cart} from '\My Codes\My Codes001\New Designs Codes\JAVASCRIPT\ire product.js';*/
  
- 
+
 
  const allProducts = [
    {id:"a1",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/01.jpg",
+   image:"/My Codes/larry-repo/larry images/01.jpg",
    name: 'Mini Food Train' ,
    pricing: 9750  ,},
 
    {id:"a2",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/04.jpeg",
+   image:"/My Codes/larry-repo/larry images/04.jpeg",
    name: 'Bowl Of Icecream' ,
    pricing: 3500 ,},
 
    {id:"a3",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/06.jpg",
+   image:"/My Codes/larry-repo/larry images/06.jpg",
    name: 'Asorted & Grilled Chicken' ,
    pricing: 5000 ,},
 
    {id:"a4",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/07.jpeg",
+   image:"/My Codes/larry-repo/larry images/07.jpeg",
    name: 'Burger With Salad' ,
    pricing: 3500,},
 
    {id:"a5",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/12.jpg",
+   image:"/My Codes/larry-repo/larry images/12.jpg",
    name: 'Mini Jollof Package' ,
    pricing: 6700 ,},
 
    {id:"a6",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/11.png",
+   image:"/My Codes/larry-repo/larry images/11.png",
    name: 'Potato Fries' ,
    pricing: 2500 ,},
 
    {id:"a7",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/27.jpg",
+   image:"/My Codes/larry-repo/larry images/27.jpg",
    name: 'Mixed Fries' ,
    pricing: 7000 ,},
 
    {id:"a8",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/35.jpg",
+   image:"/My Codes/larry-repo/larry images/35.jpg",
    name: 'Spaghetti, Chicken With Sauce' ,
    pricing: 3750 ,},
 
    {id:"a9",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/02.jpg",
+   image:"/My Codes/larry-repo/larry images/02.jpg",
    name: 'Asorted Fried Rice And Turkey' ,
    pricing: 8500 ,},
 
    {id:"a10",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/24.png",
+   image:"/My Codes/larry-repo/larry images/24.png",
    name: 'Burger' ,
    pricing: 5500 ,},
 
    {id:"a11",
-    image:"/My Codes/My Codes001/New Designs Codes/larry images/23.png",
+    image:"/My Codes/larry-repo/larry images/23.png",
     name: 'Mixed Burger' ,
     pricing: 7000 ,},
 
    {id:"a12",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/22.jpeg",
+   image:"/My Codes/larry-repo/larry images/22.jpeg",
    name: 'Creamy Youghut' ,
    pricing: 3500 ,},
 
    {id:"a13",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/13.jpg",
+   image:"/My Codes/larry-repo/larry images/13.jpg",
    name: 'Complete Food Train' ,
    pricing: 15000 ,},
 
    {id:"a14",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/14.jpg",
+   image:"/My Codes/larry-repo/larry images/14.jpg",
    name: 'Egusi Soup Package' ,
    pricing: 7000 ,},
 
    {id:"a15",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/15.jpg",
+   image:"/My Codes/larry-repo/larry images/15.jpg",
    name: 'Meatpies, Fries, Hotdog, Cookies etc' ,
    pricing: 4500 ,},
 
    {id:"a16",
-    image:"/My Codes/My Codes001/New Designs Codes/larry images/16.jpg",
+    image:"/My Codes/larry-repo/larry images/16.jpg",
     name: 'Cakes & Breads' ,
     pricing: 3500 ,},
 
    {id:"a17",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/17.jpg",
+   image:"/My Codes/larry-repo/larry images/17.jpg",
    name: 'Doughnut, Meatpies, Springroll' ,
    pricing: 3700 ,},
 
    {id:"a18",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/18.jpg",
+   image:"/My Codes/larry-repo/larry images/18.jpg",
    name: 'Chicken, Potato Fries & Curry' ,
    pricing: 7500 ,},
 
    {id:"a19",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/19.jpg",
+   image:"/My Codes/larry-repo/larry images/19.jpg",
    name: 'Milky Cookies' ,
    pricing:2500 ,},
 
    {id:"a20",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/20.jpg",
+   image:"/My Codes/larry-repo/larry images/20.jpg",
    name: 'Rice, Plantain & Peppersoup' ,
    pricing: 7000 ,},
 
    {id:"a21",
-    image:"/My Codes/My Codes001/New Designs Codes/larry images/21.jpg",
+    image:"/My Codes/larry-repo/larry images/21.jpg",
     name: 'Mixed Fries With Spiced Ponmo' ,
     pricing: 3500 ,},
 
    {id:"a22",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/03.jpg",
+   image:"/My Codes/larry-repo/larry images/03.jpg",
    name: 'Fresh Peanut Fries' ,
    pricing: 1500 ,},
 
    {id:"a23",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/28.jpg",
+   image:"/My Codes/larry-repo/larry images/28.jpg",
    name: 'Banana Bread' ,
    pricing: 1300 ,},
 
    {id:"a24",
-   image:"/My Codes/My Codes001/New Designs Codes/larry images/25.jpeg",
+   image:"/My Codes/larry-repo/larry images/25.jpeg",
    name: 'Onion & Tomato Spiced Pizza' ,
    pricing: 5500 ,},
+
+   {id:"a25",
+    image:"/My Codes/larry-repo/larry images/29.jpg",
+    name: 'Coconut Candies' ,
+    pricing: 1250,},
+
+   {id:"a26",
+   image:"/My Codes/larry-repo/larry images/30.jpg",
+   name: 'Spaghetti ' ,
+   pricing: 1500 ,},
+
+   {id:"a27",
+   image:"/My Codes/larry-repo/larry images/31.jpg",
+   name: 'Perfectly Fried & Spiced Kulikuli' ,
+   pricing: 950 ,},
+
+   {id:"a28",
+   image:"/My Codes/larry-repo/larry images/32.png",
+   name: 'Tiger Nut Drink' ,
+   pricing: 500 ,},
 
 
 
